b08d1a36879a36b3d8f613408a1e392bb3d9871d
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-23 00:26:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_writer :id 
   def initialize(name, grade)
     @name = name 
     @grade = grade 
